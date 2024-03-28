d392d4383b63fa1d1f265905ae6d0cebbae52b17
poff33rd2 <149134778+poff33rd2@users.noreply.github.com>
2024-03-28 01:04:09 -0500

    Updt 328
    todo list now features:
    - todo list counter
    - todo list check box for tracking completion
    - no bullet points (on-page css)

--- TodoList/TodoItem.cs
+++ TodoList/TodoItem.cs
@@ -1,7 +1,8 @@
-namespace BlazorSample;
+namespace TodoListItem;
 
-public class TodoItem
+public class TodoItem //creates a public class that is a blueprint for creating objects. 
 {
-    public string? Title { get; set; }
-    public bool IsDone { get; set; }
+    public string? Title { get; set; } //creates a public property - data type is string? the ? allows it to hold data.
+    public bool IsDone { get; set; } //getter retrieves data, allows private access to input outside the class, 
+    //setters allows the settings of a value to the property, allowing the assignemtn of data to private inputs
 }
